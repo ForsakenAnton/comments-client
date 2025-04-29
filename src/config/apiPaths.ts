ca100cae0bcce4daed929cbfb2b1bd57a15e2127
Anton <forsakenanton@gmail.com>
2025-04-29 12:43:08 +0300

    Work with api base address

--- src/config/apiPaths.ts
+++ src/config/apiPaths.ts
@@ -1,5 +1,4 @@
-// const API_BASE_URL = "https://localhost:7092";
-const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = "https://localhost:7092";
 const COMMENTS_SEGMENT = "api/comments";
 
 export const apiPaths = {
